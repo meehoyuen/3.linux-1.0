be69b60058ca52e795cae3379d7c562b9ce36130
meeho <meeho@meeho.com>
2018-12-23 12:45:22 +0000

    tmp:tools/build.c has bugs, elf header of zBoot/zSystem wrong. lss start_stack panic Signed-off-by: meeho <meeho@meeho.com>

--- tools/build.c
+++ tools/build.c
@@ -121,19 +121,7 @@ int main(int argc, char ** argv)
 	if ((id=open(argv[1],O_RDONLY,0))<0)
 		die("Unable to open 'boot'");
 	if (read(id,buf,MINIX_HEADER) != MINIX_HEADER)
-		;//die("Unable to read header of 'boot'");
-	if (((long *) buf)[0]!=intel_long(0x04100301))
-		;//die("Non-Minix header of 'boot'");
-	if (((long *) buf)[1]!=intel_long(MINIX_HEADER))
-		;//die("Non-Minix header of 'boot'");
-	if (((long *) buf)[3] != 0)
-		;//die("Illegal data segment in 'boot'");
-	if (((long *) buf)[4] != 0)
-		;//die("Illegal bss in 'boot'");
-	if (((long *) buf)[5] != 0)
-		;//die("Non-Minix header of 'boot'");
-	if (((long *) buf)[7] != 0)
-		;//die("Illegal symbol table in 'boot'");
+		die("Unable to read header of 'boot'");
 	i=read(id,buf,sizeof buf);
 	fprintf(stderr,"Boot sector %d bytes.\n",i);
 	if (i != 512)
@@ -151,18 +139,6 @@ int main(int argc, char ** argv)
 		die("Unable to open 'setup'");
 	if (read(id,buf,MINIX_HEADER) != MINIX_HEADER)
 		die("Unable to read header of 'setup'");
-	if (((long *) buf)[0]!=intel_long(0x04100301))
-		;//die("Non-Minix header of 'setup'");
-	if (((long *) buf)[1]!=intel_long(MINIX_HEADER))
-		;//die("Non-Minix header of 'setup'");
-	if (((long *) buf)[3] != 0)
-		;//die("Illegal data segment in 'setup'");
-	if (((long *) buf)[4] != 0)
-		;//die("Illegal bss in 'setup'");
-	if (((long *) buf)[5] != 0)
-		;//die("Non-Minix header of 'setup'");
-	if (((long *) buf)[7] != 0)
-		;//die("Illegal symbol table in 'setup'");
 	for (i=0 ; (c=read(id,buf,sizeof buf))>0 ; i+=c )
 		if (write(1,buf,c)!=c)
 			die("Write call failed");
