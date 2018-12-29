25e3b33a3757953d559158e96724e696e70c5b29
meeho <meeho@meeho>
2018-12-29 12:48:28 +0000

    add dlx linux for ext2 Signed-off-by: meeho <meeho@meeho>

--- cmt.sh
+++ cmt.sh
@@ -9,3 +9,6 @@ echo "set nu" >> ~/.vimrc
 echo "alias gg=\"grep -R --color=auto \"" >> ~/.bashrc
 git config --global user.email "meeho@meeho"
 git config --global user.name "meeho"
+if [ ! -e dlx/hd.img ];then
+    cp -fr dlx/hd10m.img dlx/hd.img
+fi
