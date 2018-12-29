fbd90e9ed314a4b97f8c7e6cdaca654a5936c711
meeho <meeho@meeho>
2018-12-29 01:56:30 +0000

    add autoconf.h Signed-off-by: meeho <meeho@meeho>

---
+++ include/linux/autoconf.h
@@ -0,0 +1,80 @@
+/*
+ * Automatically generated C config: don't edit
+ */
+
+/* 
+ * General setup
+ * 
+ */
+#define CONFIG_MATH_EMULATION 1
+#define CONFIG_BLK_DEV_HD 1
+#undef CONFIG_BLK_DEV_XD
+#undef CONFIG_INET
+#define CONFIG_MAX_16M 1
+#define CONFIG_SYSVIPC 1
+#undef CONFIG_M486
+
+/* 
+ * Program binary formats
+ * 
+ */
+#undef CONFIG_BINFMT_ELF
+#undef CONFIG_BINFMT_COFF
+
+/* 
+ * SCSI support
+ * 
+ */
+#undef CONFIG_SCSI
+
+/* 
+ * Network device support
+ * 
+ */
+#undef CONFIG_ETHERCARDS
+
+/* 
+ */
+#undef CONFIG_CDU31A
+#undef CONFIG_MCD
+#undef CONFIG_SBPCD
+
+/* 
+ * Filesystems
+ * 
+ */
+#undef CONFIG_MINIX_FS
+#undef CONFIG_EXT_FS
+#define CONFIG_EXT2_FS 1
+#undef CONFIG_XIA_FS
+#undef CONFIG_MSDOS_FS
+#undef CONFIG_PROC_FS
+#undef CONFIG_NFS_FS
+#undef CONFIG_ISO9660_FS
+#undef CONFIG_HPFS_FS
+#undef CONFIG_SYSV_FS
+
+/* 
+ * character devices
+ * 
+ */
+#undef CONFIG_PRINTER
+#undef CONFIG_BUSMOUSE
+#undef CONFIG_PSMOUSE
+#undef CONFIG_MS_BUSMOUSE
+#undef CONFIG_ATIXL_BUSMOUSE
+#undef CONFIG_SELECTION
+#undef CONFIG_TAPE_QIC02
+#undef CONFIG_FTAPE
+
+/* 
+ * Sound
+ * 
+ */
+#undef CONFIG_SOUND
+
+/* 
+ * Kernel hacking
+ * 
+ */
+#undef CONFIG_PROFILE
